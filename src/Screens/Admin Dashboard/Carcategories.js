1789e9faf73c8939420165b22e16bd01b0af72ed
Kartik Pawar <knpawar789@gmail.com>
2021-06-14 20:54:15 +0530

    handover

--- src/Screens/Admin Dashboard/Carcategories.js
+++ src/Screens/Admin Dashboard/Carcategories.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
-import { addcompany } from "../../Helpers/carcategory";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { addcompany, getcars, getcompany } from "../../Helpers/carcategory";
 
 const Carcategories = () => {
   const [categoriesInputs, setCategoriesInputs] = useState({
@@ -8,26 +8,64 @@ const Carcategories = () => {
     car: "",
     variant: "",
   });
+  const [idHolder, setIdHolder] = useState({
+    companyId: "",
+    carId: "",
+  });
+
+  const { companyId, carId } = idHolder;
 
   const { company, car, variant } = categoriesInputs;
   const [carList, setCarList] = useState([]);
   const [companyList, setCompanyList] = useState([]);
 
+  const [reRenderCars, setReRenderCars] = useState(false);
+  const [reRenderCompanies, setReRenderCompanies] = useState(false);
+
+  useEffect(() => {
+    getcompany()
+      .then((data) => {
+        if (data.error) {
+          return setCompanyList([]);
+        }
+        return setCompanyList(data);
+      })
+      .catch((err) => console.log(err));
+  }, [reRenderCompanies]);
+  useEffect(() => {
+    carId !== "" &&
+      getcars(carId)
+        .then((data) => {
+          if (data.error) {
+            return setCarList([]);
+          }
+          return setCarList(data);
+        })
+        .catch((err) => console.log(err));
+  }, [reRenderCars]);
+
   const handelAddCompany = () => {
-    // addcompany(company)
-    //   .then((data) => {
-    //     if (data.error) {
-    //       return toast.warning(data.error, {
-    //         position: "bottom-right",
-    //         autoClose: 3000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: false,
-    //         draggable: true,
-    //       });
-    //     }
-    //   })
-    //   .catch((err) => console.log(err));
+    const val = { name: company };
+    addcompany(val)
+      .then((data) => {
+        if (data.error) {
+          return toast.warning(data.error, {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+          });
+        }
+        setReRenderCompanies(!reRenderCompanies);
+        setCategoriesInputs({ ...categoriesInputs, company: "" });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handelInputs = (name) => (event) => {
+    setCategoriesInputs({ ...categoriesInputs, [name]: event.target.value });
   };
 
   return (
@@ -43,6 +81,8 @@ const Carcategories = () => {
             type="text"
             className="form-control"
             placeholder="Company Name *"
+            onChange={handelInputs("company")}
+            value={company}
           />
         </div>
 
@@ -62,10 +102,19 @@ const Carcategories = () => {
       <div className="row w-100 mt-4 d-flex flex-column align-items-center">
         <div className="row">
           <div className="col-md-6">
-            <select className="form-control">
+            <select
+              className="form-control"
+              onChange={(e) => {}}
+              value={company}
+            >
               <option className="hidden" selected disabled>
                 Comapny Name
               </option>
+              {companyList?.map((comp) => (
+                <option className="hidden" value={comp._id} key={comp._id}>
+                  {comp.name}
+                </option>
+              ))}
             </select>
           </div>
           <div className="col-md-6">
@@ -119,6 +168,7 @@ const Carcategories = () => {
         </div>
         <div className="col-md-2"></div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
