3fa1b3b0b2fe52221d136cea2ed7ddb687da8ac3
Prasadrpatil <prasadrpatil25.10@gmail.com>
2021-07-14 19:16:03 +0530

    deployment changes

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -100,9 +100,7 @@ const HomeScreen = () => {
                 <div className="text w-100 text-center mb-md-5 pb-md-5">
                   <h1 className="mb-4">Fast &amp; Easy Way To Get A Car</h1>
                   <p style={{ "font-size": "18px" }}>
-                    A small river named Duden flows by their place and supplies
-                    it with the necessary regelialia. It is a paradisematic
-                    country, in which roasted parts
+                  CarBook is India's leading car search venture & largest online B2B marketplace, connecting buyers with suppliers(Sellers).
                   </p>
                   {/* <a
                     href="https://vimeo.com/45830194"
